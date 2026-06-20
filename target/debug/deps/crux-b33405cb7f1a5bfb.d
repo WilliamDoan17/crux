2a500c0d5cd1b0f6f756004d1e541a5f1272174b
WilliamDoan17 <pdoanthe96@gmail.com>
2026-06-20 19:09:56 -0400

    feat(update-all)

---
+++ target/debug/deps/crux-b33405cb7f1a5bfb.d
@@ -0,0 +1,11 @@
+/home/will/Will/Projects/crux/target/debug/deps/crux-b33405cb7f1a5bfb.d: src/main.rs src/commands.rs src/commands/create.rs src/commands/add_test.rs src/commands/remove_test.rs src/commands/update_test.rs src/commands/run.rs
+
+/home/will/Will/Projects/crux/target/debug/deps/crux-b33405cb7f1a5bfb: src/main.rs src/commands.rs src/commands/create.rs src/commands/add_test.rs src/commands/remove_test.rs src/commands/update_test.rs src/commands/run.rs
+
+src/main.rs:
+src/commands.rs:
+src/commands/create.rs:
+src/commands/add_test.rs:
+src/commands/remove_test.rs:
+src/commands/update_test.rs:
+src/commands/run.rs:
