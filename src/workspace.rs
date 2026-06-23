35d01f7658a177366968b410454dfba0ebb58fbb
WilliamDoan17 <pdoanthe96@gmail.com>
2026-06-23 12:54:55 -0400

    feat(workspace): update naming and comments

--- src/workspace.rs
+++ src/workspace.rs
@@ -25,7 +25,9 @@ pub fn create_file(path: &Path) {
 }
 
 pub fn write_file(path: &Path, content: &str) { 
-    // access file at path
+    // access and write file at path
+    // if file already exists, truncate the file and write to it
+    // if has not exist yet, just create and write to the file 
     // if encounters error, log and exit the process (stop the CLI immediately)
 
     let mut file: File = match File::create(path) {
