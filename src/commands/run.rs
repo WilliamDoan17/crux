0a2357f638c8c49117a5b844632390f798019efc
WilliamDoan17 <pdoanthe96@gmail.com>
2026-07-10 16:03:09 -0400

    feat(run): get test numbers from the test directory

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -2,6 +2,7 @@ use std::process::exit;
 use crate::workspace::{check_crux_workspace};
 use std::path::{PathBuf, Path};
 use std::process::Command;
+use std::fs;
 
 pub fn run(args: &[String]) {
     match args {
@@ -19,22 +20,20 @@ fn run_crux(input: &str) {
     // run crux problem solution
     // - check crux workspace to see any error
     // - compile the solution file
-    // - run solution file with stdin and stdout redirection
-    //  - for every file in tests/
-        //  - store all test numbers in a Vec
-        //  - if encounter a file that is not a test file (doesn't follow N.in):
-        //      - give a warning: Warning: file_name is not a test file, skipped
-        //  - for every test number in the Vect, run run_test
+    // - get the test numbers available in tests/ (get_test_numbers) as Vec<i16>
+    // - run solution file with stdin and stdout redirection  
+        //  - for every test number in the Vec, run run_test
     // - compare and log results 
     
     let path: PathBuf = PathBuf::from(input);
 
     check_crux_workspace(&path);
     
-    compile(&path);
+    compile_solution(&path);
+
 }
 
-fn compile(path: &Path) {
+fn compile_solution(path: &Path) {
     // compile the main.cpp file to bin/solution
 
     let main_path = path.join("main.cpp");
@@ -57,6 +56,74 @@ fn compile(path: &Path) {
             exit(1);
         }
     }
+}
+
+fn get_test_numbers(path: &Path) -> Vec<i16> { 
+    // get the test numbers available in a crux workspace, return as a Vec<i16>
+    // - read the directory
+    // - for every entry read:
+    //  - if error: 
+    //      - Log error, continue
+    //  - else:
+    //      - if is_file:
+    //          if in the format N.in:
+    //              - save in the Vec
+    //          else: Log error, continue
+    //      else: Log error, continue
+    //  sort the test_numbers collected and return
+
+    let test_dir_path = path.join("tests/");
+
+    let entries = match fs::read_dir(&test_dir_path) {
+        Ok(ent) => ent,
+        Err(e) => {
+            eprintln!("Error reading test directory at {:?}: {e}", test_dir_path);
+            exit(1);
+        }
+    };
+
+    let mut test_numbers: Vec<i16> = Vec::new();
+
+    for entry in entries {
+        match entry {
+            Ok(dir_entry) => {
+                let entry_path = dir_entry.path();
+
+                if !entry_path.is_file() {
+                    eprintln!("{:?} is not a file", entry_path);
+                    continue;
+
+                }
+    
+                if entry_path.extension().and_then(|e| e.to_str()) != Some("in") {
+                    eprintln!("{:?} is not a test file (not in N.in) format", entry_path);
+                    continue;
+                }
+
+                let test_num: i16 = match entry_path
+                    .file_stem()
+                    .and_then(|s| s.to_str())
+                    .and_then(|s| s.parse().ok())
+                {
+                    Some(num) => num,
+                    None => {
+                        eprintln!("{:?} is not a test file (not in N.in) format", entry_path);
+                        continue;
+                    }
+                };
+                
+                test_numbers.push(test_num); 
+            }, 
+            Err(e) => {
+                eprintln!("Error reading entry in {:?}: {e}", test_dir_path);
+                continue;
+            }
+        }
+    }
+
+    test_numbers.sort();
+
+    test_numbers
 }
 
 fn run_test(crux_path: &Path, test_path: &Path) {
